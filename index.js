4ba2ccdcfec05fb30825fde269d8b6b4f635c8f3
Kartik <kartik.arora1214@gmail.com>
2020-12-07 11:11:43 +0530

    format fix

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-const { twitchCallWithPage } = require('./twitch_streams');
-
+const { twitchCallWithPage } = require("./twitch_streams");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/gamestreams', async (req, response) => {
-    const qs = req.query;
+app.get("/gamestreams", async (req, response) => {
+  const qs = req.query;
 
-    console.log(qs);
+  console.log(qs);
 
-    
-    let streamDataRaw = await twitchCallWithPage(
-        "https://api.twitch.tv/helix/streams",
-        {
-          game_id: qs.game_id ? qs.game_id : 13389,
-          first: 100
-        }
-      );
-
-      
-      let streamData = JSON.parse(streamDataRaw);
-      let streams = streamData.data;
-      console.log(`Before Filter Got ${streams.length}`);
-      if (qs.filter) {
-        console.log(qs.filter);
-        let filters = qs.filter.split(',');
-        streams = streams.filter(eachStream => filters.some(eachFilter => eachStream.title.includes(eachFilter) )); //if one of filters hit, We return that stream
+  let streamDataRaw = await twitchCallWithPage(
+    "https://api.twitch.tv/helix/streams",
+    {
+      game_id: qs.game_id ? qs.game_id : 13389,
+      first: 100,
     }
-        console.log(`After Filter Got ${streams.length}`);
-      response.set('Content-Type', 'application/json');
-      response.send(streams);
-    
-});
+  );
 
+  let streamData = JSON.parse(streamDataRaw);
+  let streams = streamData.data;
+  console.log(`Before Filter Got ${streams.length}`);
+  if (qs.filter) {
+    let filters = qs.filter.split(",");
+    streams = streams.filter((eachStream) =>
+      filters.some((eachFilter) => eachStream.title.includes(eachFilter))
+    ); //if one of filters hit, We return that stream
+  }
+  console.log(`After Filter Got ${streams.length}`);
+  response.set("Content-Type", "application/json");
+  response.send(streams);
+});
 
 // Tell our app to listen on port 3144
 app.listen(3144, function (err) {
-    if (err) {
-        throw err
-    }
-    console.log('Server started on port 3144')
-})
+  if (err) {
+    throw err;
+  }
+  console.log("Server started on port 3144");
+});
